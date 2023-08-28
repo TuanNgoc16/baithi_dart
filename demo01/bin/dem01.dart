f87041e4729e32aebff333a4cadf933d51a4b478
Tuấn Ngọc <103101440+TuanNgoc16@users.noreply.github.com>
2023-08-28 15:24:22 +0700

    baithi

---
+++ demo01/bin/dem01.dart
@@ -0,0 +1,5 @@
+import 'package:dem01/Demo1_Basic//01-demo-helloworld.dart' as dem01;
+
+void main(List<String> arguments) {
+  print('Hello world !!!! : ${dem01.calculate()}!');
+}
---
+++ demo01/test/dem01_test.dart
@@ -0,0 +1,8 @@
+import 'package:dem01/Demo1_Basic/01-demo-helloworld.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
